a1c0cac1b8927952bee3709e19d43de1191d631c
hnguyenec <hnguyenec@gmail.com>
2024-05-31 19:48:08 +1000

    get all fields

--- src/locations/EntryEditor.tsx
+++ src/locations/EntryEditor.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paragraph } from '@contentful/f36-components';
 import { EditorAppSDK } from '@contentful/app-sdk';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import { Field, FieldWrapper } from '@contentful/default-field-editors';
 
 const Entry = () => {
   const sdk = useSDK<EditorAppSDK>();
+  // Store all the fields that are initially displayed in the app
+  const [editorFields, setEditorFields] = useState(
+    sdk.contentType.fields
+  );
+
+  console.log(sdk.contentType.fields)
   /*
      To use the cma, inject it as follows.
      If it is not needed, you can remove the next line.
   */
   // const cma = useCMA();
 
-  return <Paragraph>Hello Entry Editor Component (AppId: {sdk.ids.app})</Paragraph>;
+  return (
+    <div>TEST</div>
+  //   <FieldWrapper sdk={FieldAppSDK} name={fieldName}>
+  //   <Field sdk={FieldAppSDK} widgetId="singleLine" />
+  // </FieldWrapper>
+  );
 };
 
 export default Entry;
