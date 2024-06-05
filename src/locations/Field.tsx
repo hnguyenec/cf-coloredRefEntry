4754665a8ee487c05a068627580c97f46a1931cf
hnguyen <huy.nguyen@tal.com.au>
2024-06-05 15:11:18 +1000

    color the ref entries and showing color codes in sidebar

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Paragraph } from '@contentful/f36-components';
-import { FieldAppSDK } from '@contentful/app-sdk';
-import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import { FieldAppSDK } from "@contentful/app-sdk";
+import { MultipleEntryReferenceEditor } from "@contentful/field-editor-reference";
+import { /* useCMA, */ useSDK } from "@contentful/react-apps-toolkit";
+import styled from "@emotion/styled";
+import { useEffect } from "react";
 
+const StyledRefDiv = styled.div<{ name: string }>`
+  border: ${(props) => {
+    return props.name === "brand-specific"
+      ? "5px solid blue"
+      : props.name === "product-specific"
+      ? "5px solid yellow"
+      : props.name === "brand-product-specific"
+      ? "5px solid red"
+      : "5px solid green";
+  }};
+  border-radius: 8px;
+`;
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
+  const instanceParameters = sdk.parameters.instance; // From App Definition
+  const appliedRefEntryType =
+    instanceParameters?.referenceEntryType ?? undefined;
+
+  useEffect(() => {
+    sdk.window.startAutoResizer();
+  }, [sdk.window]);
+
   /*
      To use the cma, inject it as follows.
      If it is not needed, you can remove the next line.
@@ -13,7 +34,55 @@ const Field = () => {
   // If you only want to extend Contentful's default editing experience
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph>;
+
+  return (
+    <MultipleEntryReferenceEditor
+      viewType="link"
+      sdk={sdk}
+      isInitiallyDisabled={true}
+      hasCardEditActions={true}
+      parameters={{
+        instance: {
+          showCreateEntityAction: true,
+          showLinkEntityAction: true,
+        },
+      }}
+      renderCustomCard={(props, _, renderDefaultCard) => {
+        const entityTags = props.entity.metadata?.tags;
+        const hasBrandTags = entityTags?.some((tag) =>
+          tag.sys.id.startsWith("brand")
+        );
+        const hasProductTags = entityTags?.some((tag) =>
+          tag.sys.id.startsWith("product")
+        );
+        // construct className based on tags
+        // if has only brand tags, no product tags, className = 'brand-specific'
+        // if has only product tags, no brand tags, className = 'product-specific'
+        // if has both product tags and brand tags, className = 'brand-product-specific'
+        // if has neither product tags nor brand tags, className = 'common'
+        const className =
+          hasBrandTags && hasProductTags
+            ? "brand-product-specific"
+            : hasBrandTags
+            ? "brand-specific"
+            : hasProductTags
+            ? "product-specific"
+            : "common";
+
+        return appliedRefEntryType === props.entity.sys.contentType.sys.id ? (
+          <StyledRefDiv name={className}>
+            {
+              // @ts-expect-error
+              renderDefaultCard({ size: "small" })
+            }
+          </StyledRefDiv>
+        ) : (
+          // @ts-expect-error
+          renderDefaultCard({ size: "small" })
+        );
+      }}
+    />
+  );
 };
 
 export default Field;
--- src/locations/Sidebar.tsx
+++ src/locations/Sidebar.tsx
@@ -1,17 +1,122 @@
-import React from 'react';
-import { Paragraph } from '@contentful/f36-components';
-import { SidebarAppSDK } from '@contentful/app-sdk';
-import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
-
+import { SidebarAppSDK } from "@contentful/app-sdk";
+import { Box, Flex } from "@contentful/f36-components";
+import { /* useCMA, */ useSDK } from "@contentful/react-apps-toolkit";
+import { useEffect, useState } from "react";
 const Sidebar = () => {
   const sdk = useSDK<SidebarAppSDK>();
+  const cma = sdk.cma;
+  const instanceParameters = sdk.parameters.instance; // From App Definition
+  const appliedRefEntryType =
+    instanceParameters?.referenceEntryType ?? undefined;
+  const fields = sdk.contentType.fields;
+  // ideally, should return all the field that are of type Reference
+  // for now, assume only one reference field
+  const referenceField = fields.find(
+    (field) =>
+      field.type === "Array" &&
+      field.items?.type === "Link" &&
+      field.items.linkType === "Entry"
+  );
+  const [refEntries, setRefEntries] = useState<any[] | undefined>();
+  const [shouldDisplayColorCodes, setShouldDisplayColorCodes] = useState(false);
+
+  useEffect(() => {
+    sdk.window.startAutoResizer();
+  }, [sdk.window]);
+
+  useEffect(() => {
+    // if there is a Reference typed Field
+    if (referenceField) {
+      const onRefChanged = (value: any) => {
+        setRefEntries(value);
+      };
+
+      sdk.entry.fields[referenceField.id].onValueChanged(onRefChanged);
+      // get all children referenced entries
+      const refEntries = sdk.entry.fields[referenceField.id].getValue();
+      if (refEntries) {
+        setRefEntries(refEntries);
+      } else {
+        setRefEntries(undefined);
+      }
+    }
+  }, [referenceField, sdk.entry.fields]);
+
+  useEffect(() => {
+    if (refEntries && refEntries.length > 0) {
+      const params = {
+        query: {
+          content_type: appliedRefEntryType,
+          "sys.id[in]": refEntries.map((entry) => entry.sys.id).join(","),
+        },
+      };
+
+      cma.entry.getMany(params).then((response) => {
+        if (response.items.length > 0) {
+          setShouldDisplayColorCodes(true);
+        } else {
+          setShouldDisplayColorCodes(false);
+        }
+      });
+    } else {
+      setShouldDisplayColorCodes(false);
+    }
+  }, [appliedRefEntryType, cma.entry, refEntries]);
   /*
      To use the cma, inject it as follows.
      If it is not needed, you can remove the next line.
   */
   // const cma = useCMA();
 
-  return <Paragraph>Hello Sidebar Component (AppId: {sdk.ids.app})</Paragraph>;
+  if (shouldDisplayColorCodes) {
+    return (
+      <Flex flexDirection="column" gap="spacingS">
+        <Flex flexDirection="row" gap="spacingS" alignItems="center">
+          <Box
+            style={{
+              backgroundColor: "green",
+              height: "20px",
+              width: "20px",
+            }}
+          />
+          <span>Common across brands and products</span>
+        </Flex>
+
+        <Flex flexDirection="row" gap="spacingS" alignItems="center">
+          <Box
+            style={{
+              backgroundColor: "yellow",
+              height: "20px",
+              width: "20px",
+            }}
+          />
+          <span>Product specific</span>
+        </Flex>
+
+        <Flex flexDirection="row" gap="spacingS" alignItems="center">
+          <Box
+            style={{
+              backgroundColor: "blue",
+              height: "20px",
+              width: "20px",
+            }}
+          />
+          <span>Brand specific</span>
+        </Flex>
+
+        <Flex flexDirection="row" gap="spacingS" alignItems="center">
+          <Box
+            style={{
+              backgroundColor: "red",
+              height: "20px",
+              width: "20px",
+            }}
+          />
+          <span>Brand and Product specific</span>
+        </Flex>
+      </Flex>
+    );
+  }
 };
 
 export default Sidebar;
